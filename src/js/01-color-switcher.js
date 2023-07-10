0e252c955b9983b35257738215fa827d2f003d80
RomaChumak <romachumak355@gmail.com>
2023-07-10 19:45:22 +0300

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', startChangingColor);
+stopBtn.addEventListener('click', stopChangingColor);
+
+let intervalId;
+function startChangingColor() {
+  startBtn.disabled = true;
+  intervalId = setInterval(changeBackgroundColor, 1000);
+}
+
+function stopChangingColor() {
+  startBtn.disabled = false;
+  clearInterval(intervalId);
+}
+
+function changeBackgroundColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+}
